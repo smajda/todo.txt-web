0cfb387edf091942fd5b0546252660682b574c69
Jon Smajda <jon@smajda.com>
2009-05-12 21:30:25 -0500

    Fixed regex so lsc and lsprj work properly.
    In the last commit (a2f785d) I added '\s' to the beginning of the
    preg_replace pattern so that 'foo@bar.com' wouldn't match, but this
    meant (unnumbered) output of lsc and lsprj wouldn't match either. Now
    the pattern matches any @ or + word with leading space or at the start
    of a line.

--- includes/todo.php
+++ includes/todo.php
@@ -66,7 +66,7 @@ function run_todo($cmd) {
             );
         // make projects and contexts into links for js
         $task = preg_replace(
-                    '/(\s(@|\+)[\S]+)/', // contexts (@) and projects (+)
+                    '/((^|\s)(@|\+)[\S]+)/', // contexts (@) and projects (+)
                     '<a class="todo-tag" href="javascript:void(0);">$1</a>',
                     $task
             );
